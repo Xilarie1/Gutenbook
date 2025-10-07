b74e0473e754184e552fe410fa2157010a869f92
Xilarie1 <martin8.kodehode@gmail.com>
2025-10-07 22:54:09 +0200

    updated header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,22 @@
-export default function Header(second) {
-  return;
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header style={{ padding: "1rem", backgroundColor: "#f5f5f5" }}>
+      <h1>Book App</h1>
+      <nav style={{ marginTop: "0.5rem" }}>
+        <Link to="/" style={{ marginRight: "1rem" }}>
+          Home
+        </Link>
+        <Link to="/favorites" style={{ marginRight: "1rem" }}>
+          Favorites
+        </Link>
+      </nav>
+
+      {/* Optional category menu */}
+      {/* <CategoryMenu /> */}
+    </header>
+  );
 }
+
+export default Header;
